07ebf94e4f87bfe26b5784b2081219e233e81352
Roman Reings <Rajkumar>
2023-09-10 17:21:58 +0530

    modified file has been adde

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 PERSON1=$1
 PERSON2=$2
+DATE=$(date)
 
 echo "Hi i am learning cloud with AWS"
 echo "$PERSON1: Hi $PERSON2 am chesthunnav"
 echo "$PERSON2: Hi $PERSON1, am chesthale"
 echo "$PERSON1: Hi $PERSON2, nuv am chesthunnav"
 echo "$PERSON2: Hi $PERSON1, am chesthale"
+echo "This coversation happened on $DATE"

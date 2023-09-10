1b830a5952aeab3b00f226897d6c5fc278f68a66
Roman Reings <Rajkumar>
2023-09-10 16:57:41 +0530

    modified file has been adde

--- 01-ShellScript.sh
+++ 01-ShellScript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
 echo "Hi i am learning cloud with AWS"
-echo "Ramesh: Hi suresh am chesthunnav"
-echo "Suresh: Hi ramesh, am chesthale"
-echo "Ramesh: Hi suresh, nuv am chesthunnav"
-echo "Suresh: Hi ramesh, am chesthale"
+echo "$PERSON1: Hi $PERSON2 am chesthunnav"
+echo "$PERSON2: Hi $PERSON1, am chesthale"
+echo "$PERSON1: Hi $PERSON2, nuv am chesthunnav"
+echo "$PERSON2: Hi $PERSON1, am chesthale"

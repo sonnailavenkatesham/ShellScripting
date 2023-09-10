9b60d068ea5efbd5fe7f40654b7545c5b7d5fe17
Roman Reings <Rajkumar>
2023-09-10 17:20:26 +0530

    modified file has been adde

---
+++ 02-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "Hi i am learning cloud with AWS"
+echo "$PERSON1: Hi $PERSON2 am chesthunnav"
+echo "$PERSON2: Hi $PERSON1, am chesthale"
+echo "$PERSON1: Hi $PERSON2, nuv am chesthunnav"
+echo "$PERSON2: Hi $PERSON1, am chesthale"

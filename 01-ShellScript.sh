05aa2d22f2693a25fab4223a929081e9103e1b53
Roman Reings <Rajkumar>
2023-09-10 16:55:36 +0530

    modified file has been adde

--- 01-ShellScript.sh
+++ 01-ShellScript.sh
@@ -2,3 +2,5 @@
 echo "Hi i am learning cloud with AWS"
 echo "Ramesh: Hi suresh am chesthunnav"
 echo "Suresh: Hi ramesh, am chesthale"
+echo "Ramesh: Hi suresh, nuv am chesthunnav"
+echo "Suresh: Hi ramesh, am chesthale"

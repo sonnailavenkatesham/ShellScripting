cded933ef7714be36d2b7922bd49154a2dccc804
Roman Reings <Rajkumar>
2023-09-11 08:44:09 +0530

    modified file has been adde

--- Sql-install.sh
+++ Sql-install.sh
@@ -6,11 +6,23 @@ then
     echo "Your are not root user..please chenge it root."
     exit 1
 fi
+
 yum install mysql -y
 
 if [ $? -ne 0 ]
 then
     echo "mysql installation failute.."
+    exit 1
+else
+    echo "mysql installation Successful.."
+fi
+
+yum install postfix -y
+
+if [ $? -ne 0 ]
+then
+    echo "mysql installation failute.."
+    exit 1
 else
     echo "mysql installation Successful.."
 fi

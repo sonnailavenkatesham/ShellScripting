dc023b803a36cb9c3b5e198c06f1a57c3c8c13d7
Roman Reings <Rajkumar>
2023-09-12 16:12:29 +0530

    New files added

--- 06-loops.sh
+++ 06-loops.sh
@@ -2,8 +2,18 @@
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
+VALIDATE(){
+if [ $? -ne 0 ]
+then
+    echo -e "$R Installation....FAIDED $N"
+else
+    echo -e "$G Installation....SUCCESS $N"
+fi
+}
+
 for i in $@
 do
     yum install $i -y
-    echo -e "$G Successfully installed...$N $i"
+    VALIDATE $? $id
 done

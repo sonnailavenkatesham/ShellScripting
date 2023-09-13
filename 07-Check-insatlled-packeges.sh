76bd9af19b0c05987f01405c3ba4168ffd9ab087
Roman Reings <Rajkumar>
2023-09-13 07:41:43 +0530

    addede new file or midified existing file

--- 07-Check-insatlled-packeges.sh
+++ 07-Check-insatlled-packeges.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
 packege=$(rpm -q git)
 INSTALLATION(){
 if [ $packege -ne 0 ]
 then
     echo "intalling packeges...."
-    yum install git -y
+    yum install mysql -y
+    VALIDATE $? "mysql" 
 else
-    echo "Already Installed"
+    echo -e "$Y Already Installed. $N"
+    exit 1
 fi
 }
 VALIDATION(){
 if [ $? -ne 0 ]
 then
-    
+    echo -e "$R Installation..$2..FAILDED $N"
+else
+    echo -e "$G Installation..$2..SUCCESSFUL $N"
 }

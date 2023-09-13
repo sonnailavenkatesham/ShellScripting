10100ee5503f8fac36cc6fb363e9b5d6812269bf
Roman Reings <Rajkumar>
2023-09-13 07:27:30 +0530

    addede new file or midified existing file

--- 07-Check-insatlled-packeges.sh
+++ 07-Check-insatlled-packeges.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 packege=$(rpm -q git)
+INSTALLATION(){
 if [ $packege -ne 0 ]
 then
-    echo "intalling packeges"
+    echo "intalling packeges...."
     yum install git -y
 else
     echo "Already Installed"
 fi
+}
+VALIDATION(){
+if [ $? -ne 0 ]
+then
+    
+}

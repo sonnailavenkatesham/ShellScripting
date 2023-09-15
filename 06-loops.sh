12a43470810dae7091d9328efe086730fe7c54df
Roman Reings <Rajkumar>
2023-09-15 10:36:37 +0530

    Added files/modified

--- 06-loops.sh
+++ 06-loops.sh
@@ -6,18 +6,7 @@ Y="\e[33m"
 DATE=$(date +%F)
 CURRENT_FILE_NAME=$0
 LOGFILE=/tmp/$CURRENT_FILE_NAME-$DATE.log
-VALIDATE(){
-if [ $? -ne 0 ]
-then
-    echo -e "$R Installation..$i..FAIDED $N"
-    exit 1
-else
-    echo -e "$G Installation..$i..SUCCESS $N"
-fi
-}
-
-for i in $@
-do
-    yum install $i -y &>>$LOGFILE
-    VALIDATE $? $i
+for i in (1..10)
+do 
+    echo "$i"
 done
